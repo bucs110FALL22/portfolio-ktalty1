7be921044290968ac19ec099a1e65e9fdd2a3cd6
Keira Talty <112440043+ktalty1@users.noreply.github.com>
2022-10-17 22:44:57 +0000

    Boolean

---
+++ ch05/exercises/boolean_functions.py
@@ -0,0 +1,29 @@
+#First Function
+
+def percentage_to_letter():    #Defining a function
+  score = 0
+
+  if score >= 90:
+    score = A
+  if score 80 -< 90:
+    score = B
+  if score 70 -<80:
+    score = C
+  if score 60 -<70:
+    score = D
+  if score <60:
+    score = F
+
+print(score)
+
+#Second Function #Defining a function
+def is_passing
+  letter = None
+if letter >= C
+  return("True")
+else:
+  return("False")
+
+#Final Part
+
+score = int(input("What is your score?"))
