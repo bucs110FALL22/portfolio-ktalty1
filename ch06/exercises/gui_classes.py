ab0c7443a04e9e0290785f27dccfb276c72fdfb4
Keira Talty <112440043+ktalty1@users.noreply.github.com>
2022-10-24 21:56:09 +0000

    GUI

---
+++ ch06/exercises/gui_classes.py
@@ -0,0 +1,22 @@
+#First Class
+class Player:
+  def__init__(self):
+    self.is_small = True
+    self.starts_as_winner = False
+    self.can_jump = True
+
+#Second Class:
+class Mushrooms:
+  def__init__(self):
+    self.is_large = False
+    self.jump_height = 5
+    self.color = "red"
+
+#Third Class:
+class Cloud:
+  def__init__(self):
+    self.move_left_and_right = True
+    self.color = "white"
+    self.stays_above_Player = True
+
+
